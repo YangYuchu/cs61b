8c0b8134b0c07b638c391a572189ae7fbe3bb0e8
Jason <yangyuchu0419@gmail.com>
2020-04-27 01:22:07 +0900

    successfully drew the universe

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -27,4 +27,41 @@ public class NBody{
         }
         return p;
 	}
+
+	/** main: to draw the initial universe state
+	*/
+	public static void main(String[] args) {
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double radius = NBody.readRadius(filename);
+		Planet[] plist = NBody.readPlanets(filename);
+		/*draw the background*/
+		StdDraw.setScale(-radius, radius);
+		StdDraw.clear();
+		StdDraw.picture(0, 0, "images/starfield.jpg", 2*radius, 2*radius);
+		/*draw the planets*/
+		for (Planet p : plist){
+			p.draw();
+		}
+		StdDraw.enableDoubleBuffering();
+		for(int time = 0; time <= T; time+=dt) {
+			double[] xForces = new double[plist.length];
+			double[] yForces = new double[plist.length];
+			for (int i = 0; i<plist.length; i++){
+				xForces[i] = plist[i].calcNetForceExertedByX(plist);
+				yForces[i] = plist[i].calcNetForceExertedByY(plist);
+			}
+			for (int i = 0; i<plist.length; i++){
+				plist[i].update(dt, xForces[i], yForces[i]);
+			}
+		StdDraw.picture(0, 0, "images/starfield.jpg", 2*radius, 2*radius);
+		for (Planet p : plist){
+			p.draw();
+		}
+		StdDraw.show();
+		StdDraw.pause(10);
+		}
+
+	}
 }
